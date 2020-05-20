dd7506c5723f2614817acf77cf63bafdc475b3d8
Lukoil39 <goshavasilevskiy@mail.ru>
2020-05-20 14:31:03 +0200

    RecyclerActivity. Применить RecyclerView и вывести фотографии Для 1го задания применить CardView

--- app/src/main/java/ru/leonidivankin/materialDesignLesson6/ItemDivider.java
+++ app/src/main/java/ru/leonidivankin/materialDesignLesson6/ItemDivider.java
@@ -14,26 +14,21 @@ class ItemDivider extends RecyclerView.ItemDecoration {
         divider = context.obtainStyledAttributes(attrs).getDrawable(0);
     }
 
-    // Рисование разделителей элементов списка в RecyclerView
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent,
                            RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
 
-        // Вычисление координат x для всех разделителей
 //        int left = parent.getPaddingLeft();
-        int left = parent.getPaddingLeft() + 100;
+        int left = parent.getPaddingLeft();
         int right = parent.getWidth() - parent.getPaddingRight();
 
-        // Для каждого элемента, кроме последнего, нарисовать линию
         for (int i = 0; i < parent.getChildCount() - 1; ++i) {
-            View item = parent.getChildAt(i);// Получить i-й элемент списка
+            View item = parent.getChildAt(i);
 
-            // Вычисление координат y текущего разделителя
             int top = item.getBottom() + ((RecyclerView.LayoutParams) item.getLayoutParams()).bottomMargin;
             int bottom = top + divider.getIntrinsicHeight();
 
-            // Рисование разделителя с вычисленными границами
             divider.setBounds(left, top, right, bottom);
             divider.draw(c);
         }
--- app/src/main/java/ru/leonidivankin/materialDesignLesson6/MainActivity.java
+++ app/src/main/java/ru/leonidivankin/materialDesignLesson6/MainActivity.java
@@ -2,6 +2,7 @@ package ru.leonidivankin.materialDesignLesson6;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
